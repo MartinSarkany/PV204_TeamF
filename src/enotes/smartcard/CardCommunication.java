ced73484b4bb78962673aa7cb8e237500d0fca15
MartinSarkany <martinsarkany@gmail.com>
2016-05-01 00:29:12 +0200

    CardCommunication complete - absolutely not tested [29-04-16]

--- src/enotes/smartcard/CardCommunication.java
+++ src/enotes/smartcard/CardCommunication.java
@@ -2,13 +2,24 @@ package enotes.smartcard;
 
 import enotes.smartcard.applet.EnotesApplet;
 import java.math.BigInteger;
+import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
+import java.security.KeyPairGenerator;
 import java.security.spec.RSAPublicKeySpec;
-import javacard.framework.Util;
-import javacard.security.PublicKey;
-import javacard.security.RSAPublicKey;
+import javacard.framework.Util;         //javacard?
+import java.security.KeyPair;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.PrivateKey;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javacard.security.PublicKey;     //javacard?
+import javacard.security.RSAPublicKey;  //javacard?
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
 import javax.smartcardio.ResponseAPDU;
 
 /**
@@ -154,5 +165,148 @@ public class CardCommunication {
             return false;
         } 
         return true;
+    }
+        
+    
+    //whatToDo: INS_VERIFYPIN to verify PIN or INS_CHANGEPIN to change PIN
+    public boolean doStuffWithPIN(byte pin[], byte whatToDo) {
+        // todo: get public key, encrypt PIN, send to card for verification
+        
+        PublicKey publicKey = generateKeyPair();
+        byte encryptedPin[];
+        if (publicKey == null) {
+            // For debugging
+            System.out.println("Public key is null");
+            return false;
+        }
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/None/PKCS1Padding"/*, "BC"*/);  //not sure about BC here - bouncy castle? do we use this?
+            cipher.init(Cipher.ENCRYPT_MODE, (Key) publicKey);
+            
+            encryptedPin = cipher.doFinal(pin);
+        } catch (Exception ex) {
+            // For debugging print out exception
+            System.out.println("Exception: " + ex.getMessage());
+            return false;
+        }
+        
+        byte apdu[] = new byte[CardMngr.HEADER_LENGTH + encryptedPin.length];
+        apdu[CardMngr.OFFSET_CLA] = (byte) 0xB0;
+        apdu[CardMngr.OFFSET_INS] = whatToDo;
+        apdu[CardMngr.OFFSET_P1] = (byte) 0x00;
+        apdu[CardMngr.OFFSET_P2] = (byte) 0x00;
+        apdu[CardMngr.OFFSET_LC] = (byte) encryptedPin.length;
+
+        System.arraycopy(pin, 0, apdu, CardMngr.OFFSET_DATA, encryptedPin.length);
+        byte response[];
+        try {
+            response = cardManager.sendAPDU(apdu).getBytes();
+            if (response[response.length - 2] != (byte) 0x90 || response[response.length - 1] != (byte) 0x00) {
+                return false;
+            }
+        } catch (Exception ex) {
+            // For debugging print out exception
+            System.out.println("Exception: " + ex.getMessage());
+            return false;
+        } 
+        return true;
+    }
+    
+     public boolean changePIN(byte pin[]) {
+         return doStuffWithPIN(pin, INS_CHANGEPIN);
+     }
+     
+    //use this instead of original verifyPIN to reduce code 
+    /*public boolean verifyPIN(byte pin[]) {
+        return doStuffWithPIN(pin, INS_VERIFYPIN);
+    }*/
+  
+    public byte[] getSecretKey(){
+        KeyPairGenerator keyGen;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+        } catch (NoSuchAlgorithmException ex) {
+            // For debugging print out exception
+            System.out.println("Exception: " + ex.getMessage());
+            return null;
+        }
+        keyGen.initialize(128);
+        KeyPair keyPair = keyGen.genKeyPair();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+        byte[] modBytes = new byte[1024];
+        publicKey.getModulus(modBytes, (short) 0);
+        
+        byte apdu[] = new byte[CardMngr.HEADER_LENGTH + modBytes.length];
+        apdu[CardMngr.OFFSET_CLA] = (byte) 0xB0;
+        apdu[CardMngr.OFFSET_INS] = INS_SET_MOD;
+        apdu[CardMngr.OFFSET_P1] = (byte) 0x00;
+        apdu[CardMngr.OFFSET_P2] = (byte) 0x00;
+        apdu[CardMngr.OFFSET_LC] = (byte) modBytes.length;
+
+        System.arraycopy(modBytes, 0, apdu, CardMngr.OFFSET_DATA, modBytes.length);
+        byte response[];
+        try {
+            response = cardManager.sendAPDU(apdu).getBytes();
+            if (response[response.length - 2] != (byte) 0x90 || response[response.length - 1] != (byte) 0x00) {
+                return null;
+            }
+        } catch (Exception ex) {
+            // For debugging print out exception
+            System.out.println("Exception: " + ex.getMessage());
+            return null;
+        }
+        
+        
+        byte expBytes[] = new byte[128];
+        apdu[CardMngr.OFFSET_INS] = INS_SET_EXP_SEND_SEC_KEY;
+        apdu[CardMngr.OFFSET_LC] = (byte) expBytes.length;
+        System.arraycopy(expBytes, 0, apdu, CardMngr.OFFSET_DATA, expBytes.length);
+        try {
+            response = cardManager.sendAPDU(apdu).getBytes();
+            if (response[response.length - 2] != (byte) 0x90 || response[response.length - 1] != (byte) 0x00) {
+                return null;
+            }
+        } catch (Exception ex) {
+            // For debugging print out exception
+            System.out.println("Exception: " + ex.getMessage());
+            return null;
+        }
+        
+        byte[] encryptedSecretKey = new byte[128];
+        PrivateKey privateKey = keyPair.getPrivate();
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance("RSA/None/PKCS1Padding");
+        } catch (NoSuchAlgorithmException ex) {
+            // For debugging print out exception
+            System.out.println(ex.getMessage());
+            return null;
+        } catch (NoSuchPaddingException ex) {
+            // For debugging print out exception
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, (Key) privateKey);
+        } catch (InvalidKeyException ex) {
+            // For debugging print out exception
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        
+        byte[] secretKey;
+        try {
+            secretKey = cipher.doFinal(encryptedSecretKey);
+        } catch (IllegalBlockSizeException ex) {
+            // For debugging print out exception
+            System.out.println(ex.getMessage());
+            return null;
+        } catch (BadPaddingException ex) {
+            // For debugging print out exception
+            System.out.println(ex.getMessage());
+            return null;
+        }
+        
+        return secretKey;       
     }
 }
